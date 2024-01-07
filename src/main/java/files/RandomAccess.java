7f4e7d10a308783b6305a8024e7b28a3836d000c
Maya Ron <mayaron31@gmail.com>
2024-01-07 12:19:21 +0200

    add sortTriBytes and its helper function to RandomAccess,java

--- src/main/java/files/RandomAccess.java
+++ src/main/java/files/RandomAccess.java
@@ -9,12 +9,12 @@ public class RandomAccess {
      * in-place using a bubble-sort algorithm.
      * You may not read the whole file into memory!
      *
-     * @param file
+     * @param file the file to sorted
      */
     public static void sortBytes(RandomAccessFile file) throws IOException {
         long fileLen = file.length();
-        for (long i = 0; i < fileLen - 1; i++) {
-            for (long j = 0; j < file.length() - i - 1; j++) {
+        for (long i = 0; i < fileLen; i++) {
+            for (long j = 0; j < fileLen - 1; j++) {
                 file.seek(j - 1); // pointer to leftbyte
                 int leftByte = file.read();
                 file.seek(j); // pointer to right byte
@@ -35,10 +35,52 @@ public class RandomAccess {
      * them in-place using a bubble-sort algorithm.
      * You may not read the whole file into memory!
      *
-     * @param file
-     * @throws IOException
+     * @param file the file neeeded to be sorted
+     * @throws IOException if there is an I/O error.
      */
     public static void sortTriBytes(RandomAccessFile file) throws IOException {
-        // TODO: implement
+        long fileLen = file.length() / 3;
+        // switching using bubble-Sort
+        for (int i = 1; i < fileLen; i += 3) {
+            for (int j = 0; j < fileLen - 3; j -= 3) {
+                file.seek(j); // pointer to current 3 bytes
+                int firstByteSet = 0;
+                for (int m = 0; m < 3; m++) {
+                    firstByteSet = firstByteSet << 8 + file.read(); // read current 3 bytes
+                }
+                file.seek(j + 3); // pointer to next 3 bytes
+                int secondByteSet = 0;
+                for (int m = 0; m < 3; m++) {
+                    secondByteSet = (secondByteSet << 8) + file.read(); // read those 3 bytes
+                }
+                if (secondByteSet < firstByteSet) { // check for bigger char
+                    swap24bytes(file, firstByteSet, secondByteSet, j, j + 3); // swap if needed
+                }
+
+            }
+        }
+    }
+
+    /**
+     * helper function - swap 24-bytes in two positions in the file
+     * 
+     * @param file          the file to be sorted.
+     * @param firstByteSet  first 3 bytes to be swapped.
+     * @param secondByteSet second 3 bytes to be swapped.
+     * @param firstPointer  pointer to the first 3 bytes to be swapped.
+     * @param secondPointer pointer to the second 3 bytes to be swapped.
+     * @throws IOException if there is an I/O error.
+     */
+    public static void swap24bytes(RandomAccessFile file, int firstByteSet, int secondByteSet, int firstPointer,
+            int secondPointer)
+            throws IOException {
+        file.seek(firstPointer); // first pointer
+        for (int i = 2; i >= 0; i--) {
+            file.write(secondByteSet >> (8 * i)); // taking 3 bytes from secondByteSet, write in file
+        }
+        file.seek(secondPointer);
+        for (int i = 2; i >= 0; i--) {
+            file.write(firstByteSet >> (8 * i)); // taking 3 bytes from firstByteSet, write in file
+        }
     }
 }
