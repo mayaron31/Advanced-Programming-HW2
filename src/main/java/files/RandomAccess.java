c303fcc7310b0761188ed4b5df69d6a256760602
Maya Ron <mayaron31@gmail.com>
2024-01-07 11:19:58 +0200

    add sortBytes to RandomAccess,java

--- src/main/java/files/RandomAccess.java
+++ src/main/java/files/RandomAccess.java
@@ -12,11 +12,26 @@ public class RandomAccess {
      * @param file
      */
     public static void sortBytes(RandomAccessFile file) throws IOException {
-        // TODO: implement
+        long fileLen = file.length();
+        for (long i = 0; i < fileLen - 1; i++) {
+            for (long j = 0; j < file.length() - i - 1; j++) {
+                file.seek(j - 1); // pointer to leftbyte
+                int leftByte = file.read();
+                file.seek(j); // pointer to right byte
+                int rightByte = file.read();
+                if (rightByte != -1 && rightByte < leftByte) { // swap left and right bytes
+                    file.seek(j - 1); // go to left
+                    file.write(rightByte); // put right byte
+                    file.seek(j); // go to right
+                    file.write(leftByte); // put left byte
+                }
+            }
+        }
     }
 
     /**
-     * Treat the file as an array of unsigned 24-bit values (stored MSB first) and sort
+     * Treat the file as an array of unsigned 24-bit values (stored MSB first) and
+     * sort
      * them in-place using a bubble-sort algorithm.
      * You may not read the whole file into memory!
      *
