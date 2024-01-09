e18ec0d1833a45df22af99c1e48f944bd3df42c5
Maya Ron <mayaron31@gmail.com>
2024-01-09 09:36:15 +0200

    edit RandomAccess.java after bug3

--- src/main/java/files/RandomAccess.java
+++ src/main/java/files/RandomAccess.java
@@ -74,11 +74,11 @@ public class RandomAccess {
     public static void swap24bytes(RandomAccessFile file, int firstByteSet, int secondByteSet, int firstPointer,
             int secondPointer)
             throws IOException {
-        file.seek(firstPointer); // first pointer
+        file.seek(secondPointer); // first pointer
         for (int i = 2; i >= 0; i--) {
             file.write(secondByteSet >> (8 * i)); // taking 3 bytes from secondByteSet, write in file
         }
-        file.seek(secondPointer);
+        file.seek(firstPointer);
         for (int i = 2; i >= 0; i--) {
             file.write(firstByteSet >> (8 * i)); // taking 3 bytes from firstByteSet, write in file
         }
